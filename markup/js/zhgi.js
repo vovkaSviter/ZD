6c9421da87c023ebe1089ae85f9333d584ec0309
Vova <vovka.sviter@gmail.com>
2018-01-29 12:01:22 +0300

    Void: in progress...

--- markup/js/zhgi.js
+++ markup/js/zhgi.js
@@ -48,6 +48,13 @@ $(document).ready(function(){
             item.css('height' , itemWidth *.65);
 
         });
+        //Team
+        $(function(){
+            var phItem = $('.phItem');
+            var phWidth = phItem.width();
+
+            phItem.css('height', phWidth * .75);
+        });
 
     //Scroll to top
     $(function(){
