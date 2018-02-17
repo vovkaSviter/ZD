972af98f3cdea70736b14ff9c831acfc455fdeeb
Vova <vovka.sviter@gmail.com>
2018-02-17 18:49:16 +0300

    Void: mechanism of action - search results - is ready...

--- wpZD/search.php
+++ wpZD/search.php
@@ -21,61 +21,63 @@ get_header(); ?>
                 }
                 ?>
                 <section class="col-xs-12">
+                    <div class="row col-xs-12 col-md-10">
 
-                    <div class="wrap">
+                        <div class="wrap">
 
-                        <header class="page-header">
-                            <?php if ( have_posts() ) : ?>
-                                <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyseventeen' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-                            <?php else : ?>
-                                <h1 class="page-title"><?php _e( 'Nothing Found', 'twentyseventeen' ); ?></h1>
-                            <?php endif; ?>
-                        </header><!-- .page-header -->
+                            <header class="page-header">
+                                <?php if ( have_posts() ) : ?>
+                                    <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyseventeen' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+                                <?php else : ?>
+                                    <h1 class="page-title"><?php _e( 'Nothing Found', 'twentyseventeen' ); ?></h1>
+                                <?php endif; ?>
+                            </header><!-- .page-header -->
 
-                        <div id="primary" class="content-area">
-                            <main id="main" class="site-main" role="main">
-                                <ol>
-                                <?php
-                                if ( have_posts() ) :
-                                    /* Start the Loop */
-                                    while ( have_posts() ) : the_post();
-
-                                        /**
-                                         * Run the loop for the search to output the results.
-                                         * If you want to overload this in a child theme then include a file
-                                         * called content-search.php and that will be used instead.
-                                         */
-
-                                        ?>
-                                        <li>
-                                            <?php
-                                                get_template_part( 'template-parts/post/content', 'excerpt' );
-                                            ?>
-                                        </li>
-                                        <?php
-
-                                    endwhile; // End of the loop.
-
-                                    the_posts_pagination( array(
-                                        'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-                                        'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-                                        'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-                                    ) );
-
-                                else : ?>
-
-                                    <p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentyseventeen' ); ?></p>
+                            <div id="primary" class="content-area">
+                                <main id="main" class="site-main" role="main">
+                                    <ol>
                                     <?php
-                                    get_search_form();
+                                    if ( have_posts() ) :
+                                        /* Start the Loop */
+                                        while ( have_posts() ) : the_post();
 
-                                endif;
-                                ?>
-                                </ol>
+                                            /**
+                                             * Run the loop for the search to output the results.
+                                             * If you want to overload this in a child theme then include a file
+                                             * called content-search.php and that will be used instead.
+                                             */
 
-                            </main><!-- #main -->
-                        </div><!-- #primary -->
-                        <?php get_sidebar(); ?>
-                    </div><!-- .wrap -->
+                                            ?>
+                                            <li>
+                                                <?php
+                                                    get_template_part( 'template-parts/post/content', 'excerpt' );
+                                                ?>
+                                            </li>
+                                            <?php
+
+                                        endwhile; // End of the loop.
+
+                                        the_posts_pagination( array(
+                                            'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+                                            'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+                                            'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+                                        ) );
+
+                                    else : ?>
+
+                                        <p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentyseventeen' ); ?></p>
+                                        <?php
+                                        get_search_form();
+
+                                    endif;
+                                    ?>
+                                    </ol>
+
+                                </main><!-- #main -->
+                            </div><!-- #primary -->
+                            <?php get_sidebar(); ?>
+                        </div><!-- .wrap -->
+                    </div>
                 </section>
             </div>
         </div>
